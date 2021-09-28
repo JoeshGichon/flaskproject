4d328ad85a82a8667745e85cff0ae35e29f95abf
Joesh Gichon <joeshgichon56@gmail.com>
2021-09-28 21:17:36 +0300

    enabled users to authenticate by creating access token as well as refresh token

---
+++ src/auth.py
@@ -0,0 +1,90 @@
+from os import access
+from flask import Blueprint,request,jsonify
+import validators
+from werkzeug.security import check_password_hash,generate_password_hash
+from src.constants.status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_409_CONFLICT
+from src.database import User,db
+from flask_jwt_extended import jwt_required, create_access_token,create_refresh_token,get_jwt_identity
+
+auth = Blueprint("auth",__name__,url_prefix="/api/v1/auth")
+
+@auth.post("/register")
+def register():
+    username = request.json["username"]
+    email = request.json["email"]
+    password = request.json["password"]
+
+    if len(password)< 6:
+        return jsonify({"error":"password is too short"}),HTTP_400_BAD_REQUEST
+
+    if len(username)< 3:
+        return jsonify({"error":"username is too short"}),HTTP_400_BAD_REQUEST
+
+    if username.isalnum() or " " in username:
+        return jsonify({"error":"username should be alphanumeric, also no spaces allowed"}),HTTP_400_BAD_REQUEST
+    
+    if not validators.email(email):
+        return jsonify({"error":"Email is not valid"})
+    
+    if User.query.filter_by(email=email).first() is not None:
+        return jsonify({"error":"Email allready exist"}),HTTP_409_CONFLICT
+
+    if User.query.filter_by(username=username).first() is not None:
+        return jsonify({"error":"username allready exist"}),HTTP_409_CONFLICT
+
+    pwd_hash = generate_password_hash(password)
+    user = User(username=username,password=pwd_hash,email=email)
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({
+        "message":"user created",
+        "User":{
+            "username":username,"emai;":email
+        }
+    }),HTTP_201_CREATED
+
+    # return "User Created"
+
+@auth.get("/login")
+def login():
+    email = request.json.get["email",""]
+    password = request.json.get["password",""]
+
+    user = User.query.filter_by(email=email).first()
+
+    if user:
+        is_pass_correct = check_password_hash(user.password,password)
+        if is_pass_correct:
+            refresh = create_refresh_token(identity=user.id)
+            access = create_access_token(identity=user.id)
+            return jsonify({
+                "refresh":refresh,
+                "access":access,
+                "username":user.username,
+                "email":user.email
+            })
+        return jsonify({"error":"wrong credentials"}),HTTP_401_UNAUTHORIZED
+
+
+
+
+@auth.get("/me")
+@jwt_required()
+def me():
+    user_id = get_jwt_identity()
+    user = User.query.filter_by(id=user_id).first()
+    return jsonify({
+        "username":user.username,
+        "email":user.email
+    }),HTTP_200_OK
+
+@auth.get("/token/refresh")
+@jwt_required(refresh=True)
+def refresh_user_token():
+    identity = get_jwt_identity()
+    access = create_access_token(identity=identity)
+    return jsonify({
+        "access":access
+    }),HTTP_200_OK
+
