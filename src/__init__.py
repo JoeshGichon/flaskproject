6a0e74c26bd1d4bdf3e6030bd8ea0ab18bba7259
Joesh Gichon <joeshgichon56@gmail.com>
2021-09-29 16:25:52 +0300

    added swagger docs to the api

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from posix import environ
 from src.constants.status_codes import HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
-from flask import Flask
+from flask import Flask, config
 import os
 from src.auth import auth
 from src.bookmarks import bookmarks
@@ -9,6 +9,8 @@ from flask_jwt_extended import JWTManager
 from flask import jsonify
 from  flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager,Server
+from flasgger import Swagger
+from src.config.swagger import template,swagger_config
 
 def create_app(test_config=None):
     app = Flask(__name__,instance_relative_config=True)
@@ -16,7 +18,11 @@ def create_app(test_config=None):
         app.config.from_mapping(
             SECRET_KEY=os.environ.get("SECRET_KEY"),
             SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DB_URI"),
-            JWT_SECRET_KEY=os.environ.get("JWT_SECRET_KEY")
+            JWT_SECRET_KEY=os.environ.get("JWT_SECRET_KEY"),
+            SWAGGER={
+                "title":"jaza keja",
+                "uiversion":3
+            }
         )
     else:
         app.config.from_mapping(test_config)
@@ -32,6 +38,7 @@ def create_app(test_config=None):
     manager.add_command('db',MigrateCommand)
 
     JWTManager(app)
+    Swagger(app,config=swagger_config,template=template)
 
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
--- src/auth.py
+++ src/auth.py
@@ -5,10 +5,12 @@ from werkzeug.security import check_password_hash,generate_password_hash
 from src.constants.status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_409_CONFLICT
 from src.database import User,db
 from flask_jwt_extended import jwt_required, create_access_token,create_refresh_token,get_jwt_identity
+from flasgger import swag_from
 
 auth = Blueprint("auth",__name__,url_prefix="/api/v1/auth")
 
 @auth.post("/register")
+@swag_from("./docs/auth/register.yaml")
 def register():
     username = request.json["username"]
     email = request.json["email"]
@@ -47,6 +49,7 @@ def register():
     # return "User Created"
 
 @auth.get("/login")
+@swag_from("./docs/auth/login.yaml")
 def login():
     email = request.json.get["email",""]
     password = request.json.get["password",""]
